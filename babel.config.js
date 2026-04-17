4d67696883ab5b6cfb0dc58e039fbe2c6cb61b50
Bikram Swain <36069225+bikramswain1992@users.noreply.github.com>
2026-04-17 15:30:14 +0100

    Feature/minimization (#1)
    * Removed un-neccessary dictionary.
    
    * Updated packages
    
    * Updated contributors page
    
    * Updated urls
    
    * Updated READMME.md

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+  plugins: [
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+  ],
 };
